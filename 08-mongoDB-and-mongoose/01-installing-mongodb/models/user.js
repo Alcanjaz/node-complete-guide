243b70c0848fe7f7db5c1115ce5d67eb5721b375
Alexis Schmoker <alexiscaposch15@gmail.com>
2020-01-04 20:51:23 -0300

    added user model and its implementation

--- 08-mongoDB-and-mongoose/01-installing-mongodb/models/user.js
+++ 08-mongoDB-and-mongoose/01-installing-mongodb/models/user.js
@@ -1,16 +1,41 @@
-const Sequelize = require('sequelize');
+const mongodb = require("mongodb");
+const getDatabase = require("../util/database").getDatabase;
 
-const sequelize = require('../util/database');
+class User {
+  constructor(name, email) {
+    this.name = name;
+    this.email = email;
+  }
 
-const User = sequelize.define('user', {
-  id: {
-    type: Sequelize.INTEGER,
-    autoIncrement: true,
-    allowNull: false,
-    primaryKey: true
-  },
-  name: Sequelize.STRING,
-  email: Sequelize.STRING
-});
+  static findById(userId) {
+    const database = getDatabase();
+
+    return database
+      .collection("users")
+      .findOne({ _id: new mongodb.ObjectID(userId) })
+      .then(user => {
+        return user;
+      })
+      .catch(err =>{
+        console.log(err);
+        throw err;
+      });
+  }
+
+  save() {
+    const database = getDatabase();
+
+    return database
+      .collection("users")
+      .insertOne(this)
+      .then(result => {
+        console.log("user created");
+      })
+      .catch(err => {
+        console.log(err);
+        throw err;
+      });
+  }
+}
 
 module.exports = User;
